4cddda97ccf70270a237e640fe53c9a9c1b4a608
Tristan Shah <123.tristan.shah@gmail.com>
2023-01-27 11:21:42 -0800

    action application

--- scripts/test.jl
+++ scripts/test.jl
@@ -12,6 +12,10 @@ kwargs = Dict(
     :ambient_speed => c, 
     :tmax => 20.0, :n => 21, :dt => 0.05)
 
+function Base.:/(cyl::Cylinder, n::Real)
+    return Cylinder(cyl.x/n, cyl.y/n, cyl.r, cyl.c)
+end
+
 struct Configuration <: AbstractDesign
     scatterers::Vector{<: AbstractDesign}
 end
@@ -24,6 +28,30 @@ function GLMakie.mesh!(ax::GLMakie.Axis3, config::Configuration)
     return nothing
 end
 
+function Base.:+(config::Configuration, action::Configuration)
+    return Configuration([
+        config.scatterers[i] + action.scatterers[i] for i ∈ axes(config.scatterers, 1)
+    ])
+end
+
+function Base.:/(config::Configuration, n::Real)
+    return Configuration([
+        config.scatterers[i] / n for i ∈ axes(config.scatterers, 1)
+    ])
+end
+
+function Waves.perturb(config::Configuration, action::Configuration, dim::TwoDim)
+    return Configuration([
+        perturb(config.scatterers[i], action.scatterers[i], dim) for i ∈ axes(config.scatterers, 1)
+    ])
+end
+
+function Configuration(dim::TwoDim; M::Int, r::Real, c::Real)
+    return Configuration([
+        Cylinder(dim, r = r, c = c) for i ∈ 1:M
+    ])
+end
+
 fig = GLMakie.Figure(resolution = (1920, 1080), fontsize = 20)
 ax = GLMakie.Axis3(fig[1,1], aspect = (1, 1, 1), perspectiveness = 0.5, title = "2D Wave", xlabel = "X", ylabel = "Y", zlabel = "Z")
 
@@ -31,9 +59,11 @@ GLMakie.xlims!(ax, getbounds(sol.wave.dim.x)...)
 GLMakie.ylims!(ax, getbounds(sol.wave.dim.y)...)
 GLMakie.zlims!(ax, -1.0, 4.0)
 
-design = Configuration([Cylinder(dim, r = 0.5), Cylinder(dim, r = 0.5)])
+design = Configuration(dim, M = 10, r = 0.5, c = 0.0)
+action = Configuration(dim, M = 10, r = 0.0, c = 0.0) / 5
+new_design = design + action
 
-GLMakie.mesh!(ax, design)
+GLMakie.mesh!(ax, new_design)
 GLMakie.save("config.png", fig)
 
 # design = ParameterizedDesign(Cylinder(0.0, 0.0, 0.5, 0.0))

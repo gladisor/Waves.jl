0d920e61e8320bbe0ff3c5432d77e36be5ded038
Tristan Shah <123.tristan.shah@gmail.com>
2023-02-16 13:25:46 -0800

    minimal example

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -1,10 +1,16 @@
 using CairoMakie
+using CairoMakie: Point
 using Waves
 
 dim = TwoDim(10.0, 400)
 u = pulse(dim)
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-heatmap!(ax, dim.x, dim.y, u)
+cyl = Circle(Point(2.0, 2.0), 0.5)
+fig = Figure(resolution = (1920, 1080))
+ax = Axis(fig[1, 1], aspect = AxisAspect(1.0))
+xlims!(ax, dim.x[1], dim.x[end])
+ylims!(ax, dim.y[1], dim.y[end])
+
+heatmap!(ax, dim.x, dim.y, u, colormap = :ice)
+poly!(ax, cyl, color = :gray)
 save("heat.png", fig)

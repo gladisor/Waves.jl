00bd76b4ce7ed155600ce5706604d20cca25eb3e
Tristan Shah <123.tristan.shah@gmail.com>
2023-02-18 07:26:17 -0800

    working well

--- scripts/main.jl
+++ scripts/main.jl
@@ -1,16 +1,19 @@
 using Waves
 
 kwargs = Dict(:pml_width => 4.0, :pml_scale => 20.0, :ambient_speed => 1.0, :dt => 0.05)
-dyn = WaveDynamics(dim = TwoDim(10.0, 0.05), design = Cylinder(-3, -3, 2.0, 0.2); kwargs...)
+dyn = WaveDynamics(dim = TwoDim(10.0, 0.05), design = Cylinder(-3, -3, 1.0, 0.2); kwargs...)
 u = pulse(dyn.dim, 0.0, 0.0, 1.0)
-env = WaveEnv(u, dyn, 5)
+env = WaveEnv(u, dyn, 20)
 policy = pos_action_space(env.dyn.C.design.initial, 1.0)
 
 wave_traj = WaveSol{TwoDim}[]
 design_traj = DesignTrajectory{Cylinder}[]
 
-while env.dyn.t < env.design_steps * 100
-    sol = env(rand(policy))
+action = Cylinder(0.2, 0.2, 0.0, 0.0)
+
+while env.dyn.t < 500
+    # sol = env(rand(policy))
+    sol  = env(action)
     push!(wave_traj, sol)
     push!(design_traj, DesignTrajectory(env))
     println(time(env))

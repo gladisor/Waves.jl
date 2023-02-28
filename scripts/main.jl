d6531eca2d7c32a5a64ac12e13f5b76890da252c
Tristan Shah <123.tristan.shah@gmail.com>
2023-02-27 17:09:04 -0800

    good

--- scripts/main.jl
+++ scripts/main.jl
@@ -2,14 +2,14 @@ using Waves
 using ReinforcementLearning
 using Flux
 
-dim = TwoDim(5.0f0, 0.025f0)
+dim = TwoDim(8.0f0, 0.025f0)
 cyl = Cylinder([0.0f0, 0.0f0], 1.0f0, 0.2f0)
-kwargs = Dict(:dim => dim, :pml_width => 1.0f0, :pml_scale => 100.0f0, :ambient_speed => 1.0f0, :dt => 0.01f0)
+kwargs = Dict(:dim => dim, :pml_width => 1.0f0, :pml_scale => 100.0f0, :ambient_speed => 1.0f0, :dt => 0.02f0)
 dyn = WaveDynamics(design = cyl; kwargs...)
 wave = Wave(dim, 6)
 
 env = gpu(WaveEnv(
-    initial_condition = Pulse(dim, 3.0f0, 3.0f0, 5.0f0),
+    initial_condition = Pulse(dim, 3.0f0, 3.0f0, 10.0f0),
     iter = WaveIntegrator(wave, split_wave_pml, runge_kutta, dyn),
     design_space = design_space(cyl, 0.5f0),
     design_steps = 50,
@@ -26,6 +26,6 @@ designs = vcat(data.designs...)
 wave = env.initial_condition(wave)
 dyn_inc = WaveDynamics(;kwargs...)
 iter_inc = gpu(WaveIntegrator(wave, split_wave_pml, runge_kutta, dyn_inc))
-sol_inc = cpu(integrate(iter_inc, length(sol_tot)))
+@time sol_inc = cpu(integrate(iter_inc, length(sol_tot) - 1))
 sol_sc = sol_tot - sol_inc
 @time render!(sol_sc, designs, path = "vid_sc.mp4")

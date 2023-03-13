c2c3fb357c4c679c808d085f122cfdebcc34f545
Tristan Shah <123.tristan.shah@gmail.com>
2023-03-13 13:55:32 -0700

    works

--- scripts/test.jl
+++ scripts/test.jl
@@ -16,21 +16,6 @@ function radii_design_space(config::Scatterers, scale::Float32)
     return Scatterers(pos, radii_low, c)..Scatterers(pos, radii_high, c)
 end
 
-function circular_points(radius::Float32, spacing::Float32, num::Int)
-    θ = acos(spacing ^ 2 / (2 * radius ^ 2) - 1)
-
-    points = []
-    
-    push!(points, [radius * cos(π) radius * sin(π)])
-
-    for i ∈ 1:num
-        push!(points, [radius * cos(π + i*θ) radius * sin(π + i*θ)])
-        push!(points, [radius * cos(π - i*θ) radius * sin(π - i*θ)])
-    end
-
-    return vcat(points...)
-end
-
 function square_formation()
     points = [
          0.0f0   0.0f0;
@@ -47,13 +32,12 @@ function square_formation()
     return points * 2
 end
 
-grid_size = 5.0f0
+grid_size = 8.0f0
 elements = 256
 fields = 6
 dim = TwoDim(grid_size, elements)
 dynamics_kwargs = Dict(:pml_width => 1.0f0, :pml_scale => 70.0f0, :ambient_speed => 1.0f0, :dt => 0.01f0)
 
-# pos = circular_points(2.0f0, 2.5f0, 2)
 pos = square_formation()
 
 r = ones(Float32, size(pos, 1)) * 0.5f0
@@ -68,7 +52,7 @@ env = gpu(WaveEnv(
     cell = WaveCell(split_wave_pml, runge_kutta),
     design = config,
     space = radii_design_space(config, 0.2f0),
-    design_steps = 50,
+    design_steps = 100,
     tmax = 10.0f0;
     dim = dim,
     dynamics_kwargs...))
@@ -78,4 +62,4 @@ traj = episode_trajectory(env)
 agent = Agent(policy, traj)
 
 @time run(agent, env, StopWhenDone())
-render!(traj, path = "vid.mp4")
+@time render!(traj, path = "vid.mp4")

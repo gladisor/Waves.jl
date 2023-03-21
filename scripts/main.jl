af1567d8b8cd9a0026476422ac01accf3a4cce00
Tristan Shah <123.tristan.shah@gmail.com>
2023-03-21 11:12:29 -0700

    hi res

--- scripts/main.jl
+++ scripts/main.jl
@@ -22,7 +22,7 @@ end
 
 design_kwargs = Dict(
     :width => 1, 
-    :hight => 2, 
+    :hight => 1, 
     :spacing => 1.0f0, 
     :r => 0.5f0, 
     :c => 0.20f0, 
@@ -37,7 +37,7 @@ end
 
 config = random_radii_scatterer_formation(;design_kwargs...)
 grid_size = 4.0f0
-elements = 128
+elements = 700
 fields = 6
 dim = TwoDim(grid_size, elements)
 dynamics_kwargs = Dict(:pml_width => 1.0f0, :pml_scale => 70.0f0, :ambient_speed => 1.0f0, :dt => 0.01f0)
@@ -58,4 +58,35 @@ policy = RandomDesignPolicy(action_space(env))
 
 @time data = generate_episode_data(policy, env, 1)
 ;
-# @time render!(policy, env, path = "data/vid.mp4")
+@time render!(policy, env, path = "data/vid.mp4")
+
+s, a = data;
+x = s[end].sol.total.u[50][:, :, 1]
+
+using CairoMakie
+fig = Figure(resolution = (1920, 1080))
+ax = Axis(
+    fig[1, 1],
+    backgroundcolor = :transparent,
+    leftspinevisible = false,
+    rightspinevisible = false,
+    bottomspinevisible = false,
+    topspinevisible = false,
+    xticklabelsvisible = false, 
+    yticklabelsvisible = false,
+    xgridcolor = :transparent,
+    ygridcolor = :transparent,
+    xminorticksvisible = false,
+    yminorticksvisible = false,
+    xticksvisible = false,
+    yticksvisible = false,
+    xautolimitmargin = (0.0,0.0),
+    yautolimitmargin = (0.0,0.0),
+    aspect = 1.0)
+
+heatmap!(ax, dim.x, dim.y, x, colormap = :ice, grid = false, ticks = false)
+save("u.png", fig)
+
+# p = WavePlot(dim)
+# heatmap!(p.ax, dim.x, dim.y, cpu(x), colormap = :ice)
+# save("u.png", p.fig)

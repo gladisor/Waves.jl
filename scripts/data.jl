dd4e8d2ab1dfe6a3fdf7e379ccdfd5b5b4a2cf67
Tristan Shah <123.tristan.shah@gmail.com>
2023-03-30 16:06:51 -0700

    generating data

--- scripts/data.jl
+++ scripts/data.jl
@@ -1,7 +1,6 @@
 using ReinforcementLearning
-using Flux
 using JLD2
-using ProgressMeter: @showprogress
+using CairoMakie
 
 using Waves
 
@@ -9,10 +8,10 @@ design_kwargs = Dict(:width => 1, :hight => 1, :spacing => 1.0f0, :c => 0.20f0, 
 config = random_radii_scatterer_formation(;design_kwargs...)
 
 grid_size = 4.0f0
-elements = 256
+elements = 512
 fields = 6
 dim = TwoDim(grid_size, elements)
-dynamics_kwargs = Dict(:pml_width => 1.0f0, :pml_scale => 70.0f0, :ambient_speed => 1.0f0, :dt => 0.01f0)
+dynamics_kwargs = Dict(:pml_width => 1.0f0, :pml_scale => 70.0f0, :ambient_speed => 2.0f0, :dt => 0.01f0)
 
 env = gpu(WaveEnv(
     initial_condition = PlaneWave(dim, -2.0f0, 10.0f0),
@@ -20,18 +19,31 @@ env = gpu(WaveEnv(
     cell = WaveCell(split_wave_pml, runge_kutta),
     design = config,
     random_design = () -> random_radii_scatterer_formation(;design_kwargs...),
-    space = radii_design_space(config, 0.2f0),
+    space = radii_design_space(config, 0.05f0),
     design_steps = 20,
-    tmax = 10.0f0;
+    tmax = 5.0f0;
     dim = dim,
     dynamics_kwargs...))
 
 policy = RandomDesignPolicy(action_space(env))
+# traj = episode_trajectory(env)
+# agent = Agent(policy, traj)
+# @time run(agent, env, StopWhenDone())
 
-render!(policy, env, path = "vid_$elements.mp4")
+# sol_tot = WaveSol([s.sol.total for s in traj.traces.state]...)
+# sol_inc = WaveSol([s.sol.incident for s in traj.traces.state]...)
+# sol_sc = sol_tot - sol_inc
 
-# for i in 1:10
-#     @time data = generate_episode_data(policy, env, 1)
-#     data_path = mkpath("data/episode$i")
-#     save_episode_data!(data..., path = data_path)
-# end
+# sc_energy = sol_sc.u .|> displacement .|> energy .|> sum
+
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# lines!(ax, sc_energy, label = "Scattered")
+# save("energy_elements=$(elements)_speed=$(env.total_dynamics.ambient_speed)_design_steps=$(env.design_steps).png", fig)
+# @time render!(traj, path = "vid_elements=$(elements)_speed=$(env.total_dynamics.ambient_speed)_design_steps=$(env.design_steps).mp4")
+
+for i in 1:10
+    @time data = generate_episode_data(policy, env, 1)
+    data_path = mkpath("data/elements=$(elements)_speed=$(env.total_dynamics.ambient_speed)_design_steps=$(env.design_steps)/episode$i")
+    save_episode_data!(data..., path = data_path)
+end

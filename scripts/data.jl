90e2c5c2f34bfe797b5982eb090362ce63a7af40
Tristan Shah <123.tristan.shah@gmail.com>
2023-03-30 23:55:52 -0700

    real

--- scripts/data.jl
+++ scripts/data.jl
@@ -5,29 +5,6 @@ using Flux
 
 using Waves
 
-design_kwargs = Dict(:width => 1, :hight => 1, :spacing => 1.0f0, :c => 0.20f0, :center => [0.0f0, 0.0f0])
-config = random_radii_scatterer_formation(;design_kwargs...)
-
-grid_size = 4.0f0
-elements = 512
-fields = 6
-dim = TwoDim(grid_size, elements)
-dynamics_kwargs = Dict(:pml_width => 1.0f0, :pml_scale => 70.0f0, :ambient_speed => 2.0f0, :dt => 0.01f0)
-
-env = gpu(WaveEnv(
-    initial_condition = PlaneWave(dim, -2.0f0, 10.0f0),
-    wave = build_wave(dim, fields = fields),
-    cell = WaveCell(split_wave_pml, runge_kutta),
-    design = config,
-    random_design = () -> random_radii_scatterer_formation(;design_kwargs...),
-    space = radii_design_space(config, 0.05f0),
-    design_steps = 20,
-    tmax = 5.0f0;
-    dim = dim,
-    dynamics_kwargs...))
-
-policy = RandomDesignPolicy(action_space(env))
-
 function Waves.DesignTrajectory(states::Vector{WaveEnvState}, actions::Vector{ <: AbstractDesign})
     designs = DesignTrajectory[]
 
@@ -40,25 +17,59 @@ function Waves.DesignTrajectory(states::Vector{WaveEnvState}, actions::Vector{ <
     return DesignTrajectory(designs...)
 end
 
-name = "elements=$(elements)_speed=$(env.total_dynamics.ambient_speed)_design_steps=$(env.design_steps)"
+design_kwargs = Dict(:width => 1, :hight => 1, :spacing => 1.0f0, :c => 3.1f0, :center => [0.0f0, 0.0f0])
+config = random_radii_scatterer_formation(;design_kwargs...)
 
-for i in 1:10
-    @time data = generate_episode_data(policy, env, 1)
-    data_path = mkpath(joinpath("data", name, "episode$i"))
+grid_size = 4.0f0
+elements = 512
+fields = 6
+dim = TwoDim(grid_size, elements)
 
-    states, actions = data
-    save_episode_data!(states, actions, path = data_path)
+dt = 0.01f0
+dynamics_kwargs = Dict(
+    :pml_width => 1.0f0, 
+    :pml_scale => 70.0f0, 
+    :ambient_speed => 1.5f0, 
+    :dt => dt)
 
-    sol_tot = WaveSol([s.sol.total for s in states]...)
-    sol_inc = WaveSol([s.sol.incident for s in states]...)
-    sol_sc = sol_tot - sol_inc
-    sc_energy = sol_sc.u .|> displacement .|> energy .|> sum
+n = 1000
 
-    fig = Figure()
-    ax = Axis(fig[1, 1])
-    lines!(ax, sc_energy, label = "Scattered")
-    save(joinpath(data_path, "energy.png"), fig)
+env = gpu(WaveEnv(
+    # initial_condition = PlaneWave(dim, -2.0f0, 10.0f0),
+    initial_condition = Pulse(dim, -2.0f0, 0.0f0, 10.0f0),
+    wave = build_wave(dim, fields = fields),
+    cell = WaveCell(split_wave_pml, runge_kutta),
+    design = config,
+    random_design = () -> random_radii_scatterer_formation(;design_kwargs...),
+    space = radii_design_space(config, 0.05f0),
+    design_steps = 20,
+    tmax = dt * n;
+    dim = dim,
+    dynamics_kwargs...))
 
-    designs = DesignTrajectory(states, actions)
-    render!(sol_tot, designs, path =  joinpath(data_path, "vid.mp4"))
-end
+policy = RandomDesignPolicy(action_space(env))
+
+@time render!(policy, env, path = "vid.mp4")
+
+# name = "elements=$(elements)_speed=$(env.total_dynamics.ambient_speed)_design_steps=$(env.design_steps)"
+
+# for i in 1:2
+#     @time data = generate_episode_data(policy, env, 1)
+#     data_path = mkpath(joinpath("data", name, "episode$i"))
+
+#     states, actions = data
+#     save_episode_data!(states, actions, path = data_path)
+
+#     sol_tot = WaveSol([s.sol.total for s in states]...)
+#     sol_inc = WaveSol([s.sol.incident for s in states]...)
+#     sol_sc = sol_tot - sol_inc
+#     sc_energy = sol_sc.u .|> displacement .|> energy .|> sum
+
+#     fig = Figure()
+#     ax = Axis(fig[1, 1])
+#     lines!(ax, sc_energy, label = "Scattered")
+#     save(joinpath(data_path, "energy.png"), fig)
+
+#     designs = DesignTrajectory(states, actions)
+#     render!(sol_tot, designs, path =  joinpath(data_path, "vid.mp4"))
+# end
--- scripts/test.jl
+++ scripts/test.jl
@@ -3,27 +3,38 @@ using Waves
 
 include("models.jl")
 
-data = load_episode_data.(readdir("data/episode1", join = true))
-s, a = first(data)
+path = "data/elements=256_speed=2.0_design_steps=20"
+# data = load_episode_data.(readdir(joinpath(path, "episode1"), join = true))
+# s, a = first(data)
 
-dynamics_kwargs = Dict(:pml_width => 1.0f0, :pml_scale => 70.0f0, :ambient_speed => 1.0f0, :dt => 0.01f0)
+files = readdir(joinpath(path, "episode1"), join = true)
+paths = files[occursin.(".jld2", files)]
 
-fields = 6
-elements = 256
-h_fields = 64
-z_fields = 2
-h_size = 1024
-design_size = 2 * length(vec(s.design))
-activation = tanh
+for p in paths
+    println(p)
+    data = load_episode_data(p)
+end
 
-total_encoder = WaveEncoder(fields, h_fields, z_fields, activation)
-design_encoder = DesignEncoder(design_size, h_size, elements, activation)
-total_iter = FEMIntegrator(elements, 100; grid_size = 4.0f0, dynamics_kwargs...)
-incident_encoder = WaveEncoder(fields, h_fields, z_fields + 1, activation)
-incident_iter = FEMIntegrator(elements, 100; grid_size = 4.0f0, dynamics_kwargs...)
-incident_mlp = Chain(Dense(256, 256), z -> hcat(tanh.(z[:, [1, 2]]), sigmoid(z[:, 3])))
+# data = load_episode_data.(files[occursin.(".jld2", files)])
 
-mlp = MLP(3 * elements, h_size, 2, 1, activation)
+# dynamics_kwargs = Dict(:pml_width => 1.0f0, :pml_scale => 70.0f0, :ambient_speed => 1.0f0, :dt => 0.01f0)
 
-model = LatentSigmaSeparationModel(total_encoder, incident_encoder, design_encoder, total_iter, incident_iter, incident_mlp, mlp)
-model(s, a)
+# fields = 6
+# elements = 256
+# h_fields = 64
+# z_fields = 2
+# h_size = 1024
+# design_size = 2 * length(vec(s.design))
+# activation = tanh
+
+# total_encoder = WaveEncoder(fields, h_fields, z_fields, activation)
+# design_encoder = DesignEncoder(design_size, h_size, elements, activation)
+# total_iter = FEMIntegrator(elements, 100; grid_size = 4.0f0, dynamics_kwargs...)
+# incident_encoder = WaveEncoder(fields, h_fields, z_fields + 1, activation)
+# incident_iter = FEMIntegrator(elements, 100; grid_size = 4.0f0, dynamics_kwargs...)
+# incident_mlp = Chain(Dense(256, 256), z -> hcat(tanh.(z[:, [1, 2]]), sigmoid(z[:, 3])))
+
+# mlp = MLP(3 * elements, h_size, 2, 1, activation)
+
+# model = LatentSigmaSeparationModel(total_encoder, incident_encoder, design_encoder, total_iter, incident_iter, incident_mlp, mlp)
+# model(s, a)

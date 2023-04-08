6f96d24e91f5d32786e1daf8d1c579725439b8b3
Tristan Shah <123.tristan.shah@gmail.com>
2023-04-07 22:50:14 -0700

    fixing env

--- scripts/onedim.jl
+++ scripts/onedim.jl
@@ -135,9 +135,12 @@ function (dyn::LinearWave)(wave::AbstractMatrix{Float32}, t::Float32)
 end
 
 mutable struct WaveEnv
-    wave::AbstractArray{Float32}
+    wave_total::AbstractArray{Float32}
+    wave_incident::AbstractArray{Float32}
+
     total::SplitWavePMLDynamics
     incident::SplitWavePMLDynamics
+
     time_step::Int
     dt::Float32
     integration_steps::Int
@@ -147,34 +150,62 @@ function Base.time(env::WaveEnv)
     return env.time_step * env.dt
 end
 
+function (env::WaveEnv)(action::AbstractDesign)
+    println("hello")
+    tspan = build_tspan(time(env), env.dt, env.integration_steps)
+    env.total = update_design(env.total, tspan, action)
+
+    total_iter = Integrator(runge_kutta, env.total, env.dt)
+    u = integrate(total_iter, env.wave_total, time(env), env.integration_steps)
+    env.wave_total = unbatch(u)[end]
+
+    incident_iter = Integrator(runge_kutta, env.incident, env.dt)
+    u = integrate(incident_iter, env.wave_incident, time(env), env.integration_steps)
+    env.wave_incident = unbatch(u)[end]
+
+    env.time_step += env.integration_steps
+end
+
 grid_size = 10.f0
 elements = 512
 ti = 0.0f0
 dt = 0.00002f0
-steps = 100
+steps = 10
 tf = steps * dt
 ambient_speed = 1531.0f0
 pml_width = 2.0f0
 pml_scale = ambient_speed * 50f0
 
-dim = OneDim(grid_size, elements)
+dim = TwoDim(grid_size, elements)
 g = grid(dim)
 C = ones(Float32, size(dim)...) * ambient_speed
 grad = build_gradient(dim)
 pml = build_pml(dim, pml_width, pml_scale)
 
-pulse = Pulse(dim, -4.0f0, 1.0f0)
-wave = pulse(build_wave(dim, fields = 2)) |> gpu
+pulse = Pulse(dim, -4.0f0, 0.0f0, 1.0f0)
+wave = pulse(build_wave(dim, fields = 6)) |> gpu
 
 initial = Scatterers([2.0f0 0.0f0], [2.0f0], [2120.0f0])
 action = Scatterers([0.0f0 0.0f0], [1.0f0], [0.0f0])
-design = DesignInterpolator(initial, action, ti, tf)
+design = DesignInterpolator(initial)
 
-# env = WaveEnv(
-#     wave,
-#     SplitWavePMLDynamics(design, g, ambient_speed, grad, pml),
-#     SplitWavePMLDynamics(nothing, g, ambient_speed, grad, pml),
-#     0, dt, steps)
+env = WaveEnv(
+    wave, wave,
+    SplitWavePMLDynamics(design, g, ambient_speed, grad, pml), 
+    SplitWavePMLDynamics(nothing, g, ambient_speed, grad, pml),
+    0, dt, steps)
+
+@time env(action)
+display(env.total.design.initial.r)
+display(env.total.design.Δ.r)
+display(env.total.design(time(env)).r)
+@time env(action)
+@time env(action)
+
+# fig = Figure()
+# ax = Axis(fig[1, 1], aspect = 1.0f0)
+# heatmap!(ax, dim.x, dim.y, env.wave_total[:, :, 1], colormap = :ice)
+# save("u.png", fig)
 
 # tspan = build_tspan(time(env), dt, steps)
 # env.total = update_design(env.total, tspan, action)
@@ -182,38 +213,37 @@ design = DesignInterpolator(initial, action, ti, tf)
 # u = integrate(iter, env.wave, time(env), env.integration_steps)
 # v, back = pullback(_wave -> iter(_wave, ti)[1], wave)
 
-# total.design(tspan[1])
-bc = ones(Float32, size(C))
-bc[[1, end]] .= 0.0f0
-dynamics = gpu(LinearWave(C, grad, bc))
+# # total.design(tspan[1])
+# bc = ones(Float32, size(C))
+# bc[[1, end]] .= 0.0f0
+# dynamics = gpu(LinearWave(C, grad, bc))
 
-iter = Integrator(runge_kutta, dynamics, dt)
-@time u = integrate(iter, wave, ti, steps);
+# iter = Integrator(runge_kutta, dynamics, dt)
+# @time u = integrate(iter, wave, ti, steps);
 
-l, back = pullback(_u -> sum(_u[:, 1, :] .^ 2), u)
-dloss = back(1.0f0)[1]
+# l, back = pullback(_u -> sum(_u[:, 1, :] .^ 2), u)
+# dloss = back(1.0f0)[1]
 
-total_adj = [dloss[:, :, end]]
+# total_adj = [dloss[:, :, end]]
 
-tspan = build_tspan(ti, dt, steps)
+# tspan = build_tspan(ti, dt, steps)
 
-for i in reverse(axes(u, 3))
-    _, back = pullback(_wave -> runge_kutta(dynamics, _wave, ti, dt), u[:, :, i])
-    adj = - back(dloss[:, :, i])[1]
-    push!(total_adj, adj)
-end
+# for i in reverse(axes(u, 3))
+#     _, back = pullback(_wave -> runge_kutta(dynamics, _wave, ti, dt), u[:, :, i])
+#     adj = - back(dloss[:, :, i])[1]
+#     push!(total_adj, adj)
+# end
 
-total_adj = sum(total_adj) * dt
+# total_adj = sum(total_adj) * dt
 
-using CairoMakie
+# using CairoMakie
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-# lines!(ax, u[:, 1, 1])
-lines!(ax, total_adj[:, 2])
-save("grad.png", fig)
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# lines!(ax, total_adj[:, 2])
+# save("grad.png", fig)
 
-# # sol = WaveSol(dim, build_tspan(ti, dt, steps), unbatch(u)) |> cpu
-# # ;
-# # # DesignTrajectory(design, steps)
-# # @time render!(sol, path = "vid.mp4", seconds = 1.0f0)
+# # # sol = WaveSol(dim, build_tspan(ti, dt, steps), unbatch(u)) |> cpu
+# # # ;
+# # # # DesignTrajectory(design, steps)
+# # # @time render!(sol, path = "vid.mp4", seconds = 1.0f0)

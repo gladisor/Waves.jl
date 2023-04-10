cb35c1c621b8ec6311abb52f81de5e2444c12d3f
Tristan Shah <123.tristan.shah@gmail.com>
2023-04-10 01:02:20 -0700

    differentiable layer

--- scripts/adjoint.jl
+++ scripts/adjoint.jl
@@ -1,7 +1,7 @@
 using CairoMakie
 using Flux
 using Flux.Losses: mse
-using Flux: jacobian, flatten, Recur, batch, unbatch, pullback, withgradient, mean
+using Flux: jacobian, flatten, Recur, batch, unbatch, pullback, withgradient, mean, @adjoint
 using Waves
 
 include("dynamics.jl")
@@ -18,24 +18,24 @@ function standard_gradient(iter::Integrator, ui::AbstractMatrix{Float32})
     return e, gs
 end
 
-function continuous_backprop(iter::Integrator, wave::AbstractMatrix{Float32}, adj::AbstractMatrix{Float32})
+# function continuous_backprop(iter::Integrator, wave::AbstractMatrix{Float32}, adj::AbstractMatrix{Float32})
     
-    tspan = build_tspan(iter.ti, iter.dt, iter.steps)
-    iter_reverse = reverse(iter)
+#     tspan = build_tspan(iter.ti, iter.dt, iter.steps)
+#     iter_reverse = reverse(iter)
 
-    wave = deepcopy(wave)
-    adj = deepcopy(adj)
+#     wave = deepcopy(wave)
+#     adj = deepcopy(adj)
 
-    for i in reverse(axes(tspan, 1))
-        _, back = pullback(_wave -> iter(_wave, tspan[i]), wave)
-        du = iter_reverse(wave, tspan[i])
+#     for i in reverse(axes(tspan, 1))
+#         _, back = pullback(_wave -> iter(_wave, tspan[i]), wave)
+#         du = iter_reverse(wave, tspan[i])
 
-        adj = adj .+ back(adj)[1]
-        wave = wave .+ du
-    end
+#         adj = adj .+ back(adj)[1]
+#         wave = wave .+ du
+#     end
 
-    return wave, adj
-end
+#     return wave, adj
+# end
 
 # function continuous_backprop(iter::Integrator, u::AbstractArray{Float32, 3}, adj::AbstractMatrix{Float32})
 
@@ -51,6 +51,8 @@ end
 # end
 
 function continuous_backprop(iter::Integrator, wave::AbstractMatrix{Float32}, adj::AbstractArray{Float32, 3})
+    println("Hello")
+
     tspan = build_tspan(iter.ti, iter.dt, iter.steps)
     iter_reverse = reverse(iter)
 
@@ -68,11 +70,23 @@ function continuous_backprop(iter::Integrator, wave::AbstractMatrix{Float32}, ad
     return dropdims(sum(batch(gs), dims = 3), dims = 3)
 end
 
+@adjoint function (iter::Integrator)(ui::AbstractMatrix{Float32})
+
+    u = iter(ui)
+    uf = u[:, :, end]
+
+    f = function(adj::AbstractArray{Float32, 3}) 
+        return (nothing, continuous_backprop(iter, uf, adj))
+    end
+
+    return u, f
+end
+
 grid_size = 10.f0
 elements = 1024
 ti = 0.0f0
 dt = 0.00002f0
-steps = 200
+steps = 50
 
 ambient_speed = 1531.0f0
 pulse_intensity = 1.0f0
@@ -87,42 +101,53 @@ ui = pulse(ui)
 dynamics = LinearWave(ambient_speed, grad, dirichlet(dim))
 iter = Integrator(runge_kutta, dynamics, ti, dt, steps)
 
+u, back = pullback(iter, ui)
+back(u)[1]
+
 opt = Momentum(1e-8)
 
 for i in 1:100
-    u = iter(ui)
-    uf = u[:, :, end]
-    e, back = pullback(_u -> Flux.mean(sum(_u[:, 1, :] .^ 2, dims = 1), dims = 2), u)
-    adj = back(one(e))[1]
 
-    adj_0 = continuous_backprop(iter, uf, adj)
+    u, back2 = pullback(iter, ui)
+    e, back1 = pullback(_u -> Flux.mean(sum(_u[:, 1, :] .^ 2, dims = 1), dims = 2), u)
+    adj_0 = back2(back1(one(e))[1])[1]
+
+    # u = iter(ui)
+    # uf = u[:, :, end]
+    # e, back = pullback(_u -> Flux.mean(sum(_u[:, 1, :] .^ 2, dims = 1), dims = 2), u)
+    # adj = back(one(e))[1]
+    
+
+
+
+    # adj_0 = continuous_backprop(iter, uf, adj)
     Flux.Optimise.update!(opt, ui, adj_0) ## low wave speed
     println(e)
 end
 
-u = iter(ui)
+# u = iter(ui)
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-xlims!(ax, dim.x[1], dim.x[end])
-ylims!(ax, -1.0f0, 1.0f0)
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# xlims!(ax, dim.x[1], dim.x[end])
+# ylims!(ax, -1.0f0, 1.0f0)
 
-record(fig, "u.mp4", axes(u, 3)) do i
-    empty!(ax)
-    lines!(ax, dim.x, u[:, 1, i], color = :blue)
-    println(i)
-end
+# record(fig, "u.mp4", axes(u, 3)) do i
+#     empty!(ax)
+#     lines!(ax, dim.x, u[:, 1, i], color = :blue)
+#     println(i)
+# end
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-xlims!(ax, dim.x[1], dim.x[end])
-ylims!(ax, minimum(u[:, 2, :]), maximum(u[:, 2, :]))
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# xlims!(ax, dim.x[1], dim.x[end])
+# ylims!(ax, minimum(u[:, 2, :]), maximum(u[:, 2, :]))
 
-record(fig, "v.mp4", axes(u, 3)) do i
-    empty!(ax)
-    lines!(ax, dim.x, u[:, 2, i], color = :blue)
-    println(i)
-end
+# record(fig, "v.mp4", axes(u, 3)) do i
+#     empty!(ax)
+#     lines!(ax, dim.x, u[:, 2, i], color = :blue)
+#     println(i)
+# end
 
-save("results/u.png", lines(dim.x, ui[:, 1], title = "Displacement"))
-save("results/v.png", lines(dim.x, ui[:, 2]))
+# save("results/u.png", lines(dim.x, ui[:, 1], title = "Displacement"))
+# save("results/v.png", lines(dim.x, ui[:, 2]))

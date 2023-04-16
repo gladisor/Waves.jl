36c6da23f3107c3ecd39ca07d69ef6e01036b765
Tristan Shah <123.tristan.shah@gmail.com>
2023-04-15 23:46:40 -0700

    computing grads

--- scripts/model.jl
+++ scripts/model.jl
@@ -1,6 +1,11 @@
 using CairoMakie
 using Flux
-using Flux: unbatch
+using Flux: unbatch, mse
+
+using Flux: pullback
+using ChainRulesCore
+
+
 using Interpolations
 using Interpolations: Extrapolation
 using Waves
@@ -10,6 +15,59 @@ using Flux: Params, Recur
 using Waves: speed
 include("../src/dynamics.jl")
 include("env.jl")
+
+struct LatentPMLWaveDynamics <: AbstractDynamics
+    ambient_speed::Float32
+    pml_scale::Float32
+
+    pml::AbstractArray
+    grad::AbstractMatrix
+    bc::AbstractArray
+end
+
+Flux.@functor LatentPMLWaveDynamics
+Flux.trainable(dyn::LatentPMLWaveDynamics) = (;dyn.pml)
+
+function LatentPMLWaveDynamics(dim::AbstractDim; ambient_speed::Float32, pml_scale::Float32)
+    pml = zeros(Float32, size(dim)...)
+    grad = build_gradient(dim)
+    bc = dirichlet(dim)
+    return LatentPMLWaveDynamics(ambient_speed, pml_scale, pml, grad, bc)
+end
+
+function (dyn::LatentPMLWaveDynamics)(u::AbstractMatrix{Float32}, t::Float32)
+    U = u[:, 1]
+    V = u[:, 2]
+    C = u[:, 3] * dyn.ambient_speed
+
+    ∇ = dyn.grad
+    σ = dyn.pml * dyn.pml_scale
+
+    du = C .^ 2 .* ∂x(∇, V) .- σ .* U
+    dv = ∂x(∇, U) .- σ .* V
+    dc = C * 0.0f0
+
+    return hcat(dyn.bc .* du, dv, dc)
+end
+
+struct WaveControlModel
+    wave_encoder::WaveEncoder
+    design_encoder::DesignEncoder
+    iter::Integrator
+    mlp::Chain
+end
+
+Flux.@functor WaveControlModel
+
+function encode(model::WaveControlModel, wave::AbstractArray{Float32, 3}, design::AbstractDesign, action::AbstractDesign)
+    return hcat(model.wave_encoder(wave), model.design_encoder(design, action))
+end
+
+function (model::WaveControlModel)(wave::AbstractArray{Float32, 3}, design::AbstractDesign, action::AbstractDesign)
+    zi = encode(model, wave, design, action)
+    z = model.iter(zi)
+    return model.mlp(z)
+end
 
 grid_size = 8.0f0
 elements = 256
@@ -45,33 +103,55 @@ env = ScatteredWaveEnv(
     dt,
     steps)
 
-designs = []
-sigma = []
-ts = []
-us = []
-sols = []
+action = Scatterers([0.0f0 1.0f0], [0.0f0], [0.0f0])
+sol = env(action)
+sigma = env.σ
 
-for i in 1:10
-    action = Scatterers([(-1) ^ (i + 1) * 1.0f0 (-1) ^ i * 1.0f0], [0.0f0], [0.0f0])
-    @time sol = env(action)
+ui = sol.u[1]
 
-    push!(sols, sol)
-    push!(designs, env.total.design)
-    push!(sigma, env.σ)
-end
+latent_dim = OneDim(grid_size, 1024)
+latent_dynamics = LatentPMLWaveDynamics(latent_dim, ambient_speed = ambient_speed, pml_scale = 10000.0f0)
 
-design_tspan = vcat(sols[1].t[1], [sol.t[end] for sol in sols]...)
-ds = [designs[1](0.0f0), [d(sol.t[end]) for (d, sol) in zip(designs, sols)]...]
-design_interp = linear_interpolation(design_tspan, ds)
+model = WaveControlModel(
+    WaveEncoder(6, 8, 2, relu),
+    DesignEncoder(2 * length(vec(initial)), 128, 1024, relu),
+    Integrator(runge_kutta, latent_dynamics, ti, dt, steps),
+    Chain(Flux.flatten, Dense(3072, 1), vec))
 
-sol = WaveSol(sols...)
-u_interp = linear_interpolation(sol.t, sol.u)
+# model(ui, initial, action)
 
-render!(dim, sol.t, u_interp, design_interp, seconds = 5.0f0)
+sigma, back = Flux.pullback(_model -> _model(ui, initial, action), model)
+gs = back(sigma)[1]
 
-sigma = [sigma[1][1], [s[2:end] for s in sigma]...]
+# render!(latent_dim, z, path = "vid.mp4")
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-lines!(ax, sol.t, vcat(sigma...), color = :blue)
-save("sigma2.png", fig)
+# designs = []
+# sigma = []
+# ts = []
+# us = []
+# sols = []
+
+# for i in 1:10
+#     action = Scatterers([(-1) ^ (i + 1) * 1.0f0 (-1) ^ i * 1.0f0], [0.0f0], [0.0f0])
+#     @time sol = env(action)
+
+#     push!(sols, sol)
+#     push!(designs, env.total.design)
+#     push!(sigma, env.σ)
+# end
+
+# design_tspan = vcat(sols[1].t[1], [sol.t[end] for sol in sols]...)
+# ds = [designs[1](0.0f0), [d(sol.t[end]) for (d, sol) in zip(designs, sols)]...]
+# design_interp = linear_interpolation(design_tspan, ds)
+
+# sol = WaveSol(sols...)
+# u_interp = linear_interpolation(sol.t, sol.u)
+
+# render!(dim, sol.t, u_interp, design_interp, seconds = 5.0f0)
+
+# sigma = [sigma[1][1], [s[2:end] for s in sigma]...]
+
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# lines!(ax, sol.t, vcat(sigma...), color = :blue)
+# save("sigma2.png", fig)

cd071e441c1bbb84866cc8bfece8c9ef3c54e191
Tristan Shah <123.tristan.shah@gmail.com>
2023-04-28 23:47:34 -0700

    scaling

--- scripts/test.jl
+++ scripts/test.jl
@@ -1,15 +1,22 @@
 include("dependencies.jl")
 
-dim = TwoDim(8.0f0, 256)
-pulse = build_pulse(build_grid(dim), -5.0f0, 0.0f0, 10.0f0)
+dim = TwoDim(20.0f0, 512)
+pulse = build_pulse(build_grid(dim), -10.0f0, 0.0f0, 10.0f0)
+
+
+random_radii = RandomRadiiScattererGrid(
+    width = 3, height = 5, spacing = 3.0f0, 
+    c = BRASS, center = [0.0f0, 0.0f0])
+
+ds = radii_design_space(random_radii(), 1.0f0)
 
 env = WaveEnv(
     dim, 
     reset_wave = Silence(),
-    reset_design = () -> NoDesign(),
-    action_space = NoDesign()..NoDesign(),
-    source = Source(pulse, freq = 200.0f0),
-    actions = 20) |> gpu
+    reset_design = random_radii,
+    action_space = ds,
+    source = Source(pulse, freq = 300.0f0),
+    actions = 10) |> gpu
 
 policy = RandomDesignPolicy(action_space(env))
 

de4b9b38837dc4d1829e018afe897d63da3c1308
Tristan Shah <123.tristan.shah@gmail.com>
2023-07-14 13:05:45 -0700

    Plots

--- generalization_results/test.jl
+++ generalization_results/test.jl
@@ -119,7 +119,7 @@ reset!(env)
 policy = RandomDesignPolicy(action_space(env))
 
 println("Preparing Data")
-train_data = Vector{EpisodeData}([EpisodeData(path = joinpath(data_path, "episode$i/episode.bson")) for i in 961:1000])
+# train_data = Vector{EpisodeData}([EpisodeData(path = joinpath(data_path, "episode$i/episode.bson")) for i in 961:1000])
 
 println("Loading Models")
 pml_model = gpu(BSON.load(pml_model_path)[:model])
@@ -128,99 +128,38 @@ testmode!(pml_model)
 testmode!(no_pml_model)
 
 # loss = Dict(
-#     :pml_mean => [],
-#     :pml_std => [],
-#     :no_pml_mean => [],
-#     :no_pml_std => []
+#     :pml => [],
+#     :nopml => [],
 # )
 
-loss = Dict(
-    :pml => Vector{Float32}[],
-    :no_pml => Vector{Float32}[]
-)
+# for h in horizons
+#     train_loader = DataLoader(prepare_data(train_data, h), shuffle = true, batchsize = batchsize, partial = false)
+#     states, actions, tspans, sigmas = gpu(first(train_loader))
 
-for h in horizons
-    train_loader = DataLoader(prepare_data(train_data, h), shuffle = true, batchsize = batchsize, partial = false)
-    states, actions, tspans, sigmas = gpu(first(train_loader))
+#     println("Evaluating on Batch")
+#     @time begin
+#         pml_sigmas = pml_model(states, actions, tspans)
+#         no_pml_sigmas = no_pml_model(states, actions, tspans)
+#     end
 
-    println("Evaluating on Batch")
-    @time begin
-        pml_sigmas = pml_model(states, actions, tspans)
-        no_pml_sigmas = no_pml_model(states, actions, tspans)
-    end
+#     y = flatten_repeated_last_dim(sigmas)
+#     pml_loss = Flux.mse(pml_sigmas, y)
+#     no_pml_loss = Flux.mse(no_pml_sigmas, y)
 
-    y = flatten_repeated_last_dim(sigmas)
-    pml_loss = cpu(vec(Flux.mse(pml_sigmas, y, agg = x -> Flux.mean(x, dims = 1))))
-    no_pml_loss = cpu(vec(Flux.mse(no_pml_sigmas, y, agg = x -> Flux.mean(x, dims = 1))))
-
-    push!(loss[:pml], pml_loss)
-    push!(loss[:no_pml], no_pml_loss)
-
-    # pml_mean = Flux.mean(pml_loss)
-    # pml_std = Flux.std(pml_loss)
-
-    # no_pml_mean = Flux.mean(no_pml_loss)
-    # no_pml_std = Flux.std(no_pml_loss)
-
-    # println(size(pml_loss))
-
-    # push!(loss[:pml_mean], pml_mean)
-    # push!(loss[:pml_std], pml_std)
-    # push!(loss[:no_pml_mean], no_pml_mean)
-    # push!(loss[:no_pml_std], no_pml_std)
-    # println("PML Loss: $(pml_mean)")
-    # println("NO PML Loss: $(no_pml_mean)")
-end
-
-pml_loss = hcat(loss[:pml]...)
-no_pml_loss = hcat(loss[:no_pml]...)
-
-xs = ones(Int, size(pml_loss)) .* collect(1:19)'
-fig = Figure()
-ax = Axis(fig[1, 1])
-violin!(ax, vec(xs), vec(pml_loss))
-save("density.png", fig)
-
-
-BSON.bson(
-    "loss.bson", 
-    horizon = horizons,
-    pml_loss = pml_loss, 
-    no_pml_loss = no_pml_loss)
+#     push!(loss[:pml], pml_loss)
+#     push!(loss[:nopml], no_pml_loss)
+#     println("PML Loss: $(pml_loss)")
+#     println("NO PML Loss: $(no_pml_loss)")
+# end
 
 # using CSV
 # using DataFrames
 
 # data = DataFrame(
 #     horizon = horizons,
-#     pml_mean = loss[:pml_mean],
-#     pml_std = loss[:pml_std],
-#     no_pml_mean = loss[:no_pml_mean],
-#     no_pml_std = loss[:no_pml_std]
-#     )
-
+#     pml_loss = loss[:pml],
+#     nopml_loss = loss[:nopml])
 # CSV.write("generalization.csv", data)
-
-# fig = Figure()
-# ax = Axis(fig[1, 1],
-#     title = "Effect of Increased Planning Horizon on Validation Loss",
-#     xlabel = "Horizon",
-#     ylabel = "Validation Loss")
-
-# pml_mean = Float32.(loss[:pml_mean])
-# pml_std = Float32.(loss[:pml_std])
-# no_pml_mean = Float32.(loss[:no_pml_mean])
-# no_pml_std = Float32.(loss[:no_pml_std])
-
-
-
-# scatter!(ax, horizons, pml_mean, markersize = 10, color = :blue)
-# errorbars!(ax, horizons, pml_mean, pml_std, color = :blue, whiskerwidth = 10, label = "PML")
-
-# scatter!(ax, horizons, no_pml_mean, markersize = 10, color = :orange)
-# errorbars!(ax, horizons, no_pml_mean, no_pml_std, color = :orange, whiskerwidth = 10, label = "No PML")
-# axislegend(ax)
-# save("error.png", fig)
 
 # fig = Figure()
 # ax = Axis(fig[1, 1],
@@ -233,41 +172,30 @@ BSON.bson(
 # axislegend(ax, position = :lt)
 # save("generalization.png", fig)
 
+horizon = 200
+train_loader = DataLoader(prepare_data(train_data, horizon), shuffle = true, batchsize = batchsize, partial = false)
+states, actions, tspans, sigmas = gpu(first(train_loader))
 
+pml_sigmas = pml_model(states, actions, tspans)
+no_pml_sigmas = no_pml_model(states, actions, tspans)
 
+y = flatten_repeated_last_dim(sigmas)
+pml_loss = Flux.mse(pml_sigmas, y)
+no_pml_loss = Flux.mse(no_pml_sigmas, y)
+println("PML Loss: $(pml_loss)")
+println("NO PML Loss: $(no_pml_loss)")
 
+ts = flatten_repeated_last_dim(tspans)
+fig = Figure()
+ax1 = Axis(fig[1, 1])
+ax2 = Axis(fig[1, 2])
 
+lines!(ax1, cpu(ts[:, 1]), cpu(pml_sigmas[:, 1]), label = "PML", color = :orange)
+lines!(ax1, cpu(ts[:, 1]), cpu(y[:, 1]), label = "True", color = :blue)
 
+lines!(ax2, cpu(ts[:, 1]), cpu(y[:, 1]), label = "True", color = :blue)
+lines!(ax2, cpu(ts[:, 1]), cpu(no_pml_sigmas[:, 1]), label = "NO PML", color = :green)
 
-
-
-
-
-
-# horizon = 200
-# train_loader = DataLoader(prepare_data(train_data, horizon), shuffle = true, batchsize = batchsize, partial = false)
-# states, actions, tspans, sigmas = gpu(first(train_loader))
-
-# pml_sigmas = pml_model(states, actions, tspans)
-# no_pml_sigmas = no_pml_model(states, actions, tspans)
-
-# y = flatten_repeated_last_dim(sigmas)
-# pml_loss = Flux.mse(pml_sigmas, y)
-# no_pml_loss = Flux.mse(no_pml_sigmas, y)
-# println("PML Loss: $(pml_loss)")
-# println("NO PML Loss: $(no_pml_loss)")
-
-# ts = flatten_repeated_last_dim(tspans)
-# fig = Figure()
-# ax1 = Axis(fig[1, 1])
-# ax2 = Axis(fig[1, 2])
-
-# lines!(ax1, cpu(ts[:, 1]), cpu(pml_sigmas[:, 1]), label = "PML", color = :orange)
-# lines!(ax1, cpu(ts[:, 1]), cpu(y[:, 1]), label = "True", color = :blue)
-
-# lines!(ax2, cpu(ts[:, 1]), cpu(y[:, 1]), label = "True", color = :blue)
-# lines!(ax2, cpu(ts[:, 1]), cpu(no_pml_sigmas[:, 1]), label = "NO PML", color = :green)
-
-# axislegend(ax1)
-# axislegend(ax2)
-# save("results/sigma$(horizon).png", fig)
+axislegend(ax1)
+axislegend(ax2)
+save("results/sigma$(horizon).png", fig)
--- scripts/vis.jl
+++ scripts/vis.jl
@@ -1,23 +1,64 @@
-using DataFrames
-using CSV
+# using DataFrames
+# using CSV
 using CairoMakie
+using BSON
 
-mpc = CSV.read("pml=on,latent=15m,horizon=3/data.csv", DataFrame)
-random = CSV.read("random_baseline/data.csv", DataFrame)
+# mpc = CSV.read("pml=on,latent=15m,horizon=3/data.csv", DataFrame)
+# random = CSV.read("random_baseline/data.csv", DataFrame)
 
-tspan = mpc[!, :tspan]
-mpc_avg = (mpc[!, :sigma1] .+ mpc[!, :sigma2] .+ mpc[!, :sigma3]) ./ 3.0f0
-random_avg = (random[!, :sigma1] .+ random[!, :sigma2] .+ random[!, :sigma3]) ./ 3.0f0
+# tspan = mpc[!, :tspan]
+# mpc_avg = (mpc[!, :sigma1] .+ mpc[!, :sigma2] .+ mpc[!, :sigma3]) ./ 3.0f0
+# random_avg = (random[!, :sigma1] .+ random[!, :sigma2] .+ random[!, :sigma3]) ./ 3.0f0
+
+# fig = Figure()
+# ax = Axis(
+#     fig[1, 1], 
+#     title = "Comparison of Scattered Energy Produced Over Time",
+#     xlabel = "Time (s)",
+#     ylabel = "Scattered Energy (σ)")
+    
+# lines!(ax, tspan, mpc_avg, color = :blue, label = "mpc")
+# lines!(ax, tspan, random_avg, color = :orange, label = "random")
+# axislegend(ax, position = :rb)
+# save("sigma.png", fig)
+
+data = BSON.load("loss.bson")
+horizon = data[:horizon]
+pml_loss = data[:pml_loss]
+no_pml_loss = data[:no_pml_loss]
+
+xs = vec(ones(Int, size(pml_loss)) .* horizon')
+
+dodge = ones(Int, size(pml_loss))
+dodge1 = vec(dodge)
+dodge2 = 2 * vec(dodge)
 
 fig = Figure()
-ax = Axis(
-    fig[1, 1], 
-    title = "Comparison of Scattered Energy Produced Over Time",
-    xlabel = "Time (s)",
-    ylabel = "Scattered Energy (σ)")
-    
-lines!(ax, tspan, mpc_avg, color = :blue, label = "mpc")
-lines!(ax, tspan, random_avg, color = :orange, label = "random")
-axislegend(ax, position = :rb)
-save("sigma.png", fig)
+ax = Axis(fig[1, 1], 
+    title = "Effect of Increased Planning Horizon on Validation Loss",
+    xlabel = "Planning Horizon", 
+    ylabel = "Validation Loss")
+
+boxplot!(
+    ax, 
+    xs, 
+    vec(pml_loss),
+    dodge = dodge1, 
+    show_outliers = false,
+    color = :red,
+    width = 3,
+    label = "PML")
+
+boxplot!(
+    ax,
+    xs, 
+    vec(no_pml_loss),
+    dodge = dodge2,
+    show_outliers = false,
+    color = :blue,
+    width = 3,
+    label = "No PML")
+
+axislegend(ax, position = :lt)
+save("loss.png", fig)
 

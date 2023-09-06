5c101574957e0511f7224c0de3056abcd04649de
Tristan Shah <123.tristan.shah@gmail.com>
2023-09-05 20:52:33 -0700

    works on gpu

--- scripts/wave.jl
+++ scripts/wave.jl
@@ -1,8 +1,10 @@
 using Waves
 using Flux
+Flux.CUDA.allowscalar(false)
 using CairoMakie
+using Interpolations
 
-struct AcousticDynamics{D <: AbstractDim} <: AbstractDynamics
+struct AcousticDynamics <: AbstractDynamics
     c0::Float32
     grad::AbstractMatrix{Float32}
     pml::AbstractArray{Float32}
@@ -13,24 +15,11 @@ Flux.@functor AcousticDynamics
 Flux.trainable(::AcousticDynamics) = (;)
 
 function AcousticDynamics(dim::AbstractDim, c0::Float32, pml_width::Float32, pml_scale::Float32)
-    
-    return AcousticDynamics{typeof(dim)}(
+    return AcousticDynamics(
         c0, 
         build_gradient(dim), 
         build_pml(dim, pml_width, pml_scale), 
         build_dirichlet(dim))
-end
-
-function (dyn::AcousticDynamics{OneDim})(x, t::AbstractVector{Float32}, θ)
-    f = θ
-
-    u = x[:, 1]
-    v = x[:, 2]
-
-    du = dyn.c0 .^ 2 * dyn.grad * v .- dyn.pml .* u
-    dv = dyn.grad * (u .+ f(t)) .- dyn.pml .* v
-
-    return hcat(du .* dyn.bc, dv)
 end
 
 function acoustic_dynamics(x, c, f, ∇, pml, bc)
@@ -61,7 +50,7 @@ function acoustic_dynamics(x, c, f, ∇, pml, bc)
     return cat(bc .* dU, dVx, dVy, dΨx, dΨy, dΩ, dims = 3)
 end
 
-function (dyn::AcousticDynamics{TwoDim})(x, t::AbstractVector{Float32}, θ)
+function (dyn::AcousticDynamics)(x, t::AbstractVector{Float32}, θ)
     c = C(t)
     f = F(t)
 
@@ -90,33 +79,40 @@ end
 
 dim = TwoDim(15.0f0, 256)
 dyn = AcousticDynamics(dim, WATER, 2.0f0, 10000.0f0)
-iter = Integrator(runge_kutta, dyn, 1f-5, 100)
+iter = Integrator(runge_kutta, dyn, 1f-5, 2000) |> gpu
 tspan = build_tspan(iter, 0.0f0)
-grid = build_grid(dim)
+grid = gpu(build_grid(dim))
 
 n = 1
-μ = [-10.0f0, 0.0f0]'
-σ = ones(Float32, n) * 0.3f0
-a = ones(Float32, n) * 1.0f0
+μ = gpu([-10.0f0;; 0.0f0])
+σ = gpu(ones(Float32, n) * 0.3f0)
+a = gpu(ones(Float32, n) * 1.0f0)
 
 pulse = build_normal(grid, μ, σ, a)
-F = Source(pulse, 1000.0f0)
+F = gpu(Source(pulse, 1000.0f0))
 
 design_space = Waves.build_triple_ring_design_space()
 d1 = rand(design_space)
 d2 = rand(design_space)
-design_interpolator = DesignInterpolator(d1, d2, tspan[1], tspan[end])
-C = t -> speed(design_interpolator(cpu(t[1])), grid, dyn.c0)
+design_interpolator = gpu(DesignInterpolator(d1, d2, tspan[1], tspan[end]))
+
+C = t -> speed(design_interpolator(cpu(t)[1]), grid, dyn.c0)
+C([1.0f0])
 
 θ = [C, F]
-x = build_wave(dim, 6)
-@time sol = iter(x, tspan, θ)
+x = gpu(build_wave(dim, 6))
+@time sol = cpu(iter(x, gpu(tspan), θ))
 println("Min: ", sol[:, :, 1, :] |> minimum)
 println("Max: ", sol[:, :, 1, :] |> maximum)
 
+u = linear_interpolation(tspan, Flux.unbatch(sol[:, :, 1, :]))
+
+
+idx = 1:5:length(tspan)
+
 fig = Figure()
 ax = Axis(fig[1, 1], aspect = 1.0f0)
-@time record(fig, "vid.mp4", axes(sol, 4)) do i
+@time record(fig, "vid.mp4", idx) do i
     empty!(ax)
-    heatmap!(ax, dim.x, dim.y, sol[:, :, 1, i], colormap = :ice, colorrange = (-1.0f0, 1.0f0))
+    heatmap!(ax, dim.x, dim.y, u(tspan[i]), colormap = :ice, colorrange = (-1.0f0, 1.0f0))
 end
--- src/sources.jl
+++ src/sources.jl
@@ -14,6 +14,6 @@ function (source::Source)(t::Float32)
     return source.shape * sin(2.0f0 * pi * t * source.freq)
 end
 
-function (source::Source)(t::Vector{Float32})
+function (source::Source)(t::AbstractVector{Float32})
     return source.shape .* sin.(2.0f0 * pi * permutedims(t) * source.freq)
 end

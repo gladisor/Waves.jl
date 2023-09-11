76ae8ef0f44a5657c3769da23e2cbaff4e39fbf1
Tristan Shah <123.tristan.shah@gmail.com>
2023-09-10 21:51:00 -0700

    poly interp

--- scripts/interp.jl
+++ scripts/interp.jl
@@ -3,35 +3,7 @@ using CairoMakie
 using LinearAlgebra
 using Flux
 
-function linear_interpolation(X, Y, x)
-    d = X .- x
-
-    less_than = d .< 0.0f0
-    greater_than = d .> 0.0f0
-    less_than_or_equal_to = .!greater_than
-    greater_than_or_equal_to = .!less_than
-    equal_to = less_than_or_equal_to .& greater_than_or_equal_to
-
-    if any(equal_to)
-        return equal_to, equal_to
-    else
-        _, l = findmin(less_than) .- 1
-        _, r = findmax(greater_than)
-        return l, r
-    end
-
-    # if l == r
-    #     return Y[l]
-    # else
-    #     display(l)
-    #     display(r)
-    #     dYdX = (Y[r] - Y[l]) / (X[r] - X[l])
-    #     return Y[l] + dYdX * (x - X[l])
-    # end
-end
-
 mask = I(10)
-
 function polynomial_interpolation(X, Y, x)
     scale = maximum(abs.(X))
     n = ((X .- x) .* (.!mask) .+ mask) ./ scale
@@ -39,12 +11,12 @@ function polynomial_interpolation(X, Y, x)
     numer = dropdims(prod(n, dims = 1), dims = 1)
     denom = dropdims(prod(d, dims = 1), dims = 1)
     coef = numer ./ denom
-    Y' * coef
+    return Y' * coef
 end
 
-gs = 5.0f0
-dim = OneDim(gs, 512)
-reduced_dim = OneDim(gs, 10)
+grid_size = 5.0f0
+dim = OneDim(grid_size, 512)
+reduced_dim = OneDim(grid_size, 10)
 
 x = reduced_dim.x
 y = sin.(x) .* cos.(x) .+ x .^ 2

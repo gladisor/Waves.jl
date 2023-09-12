e293ded518383fb52a12002d2faca418b0c14a6b
Tristan Shah <123.tristan.shah@gmail.com>
2023-09-11 19:44:15 -0700

    progress

--- scripts/wave.jl
+++ scripts/wave.jl
@@ -71,33 +71,36 @@ struct PolynomialInterpolation
 end
 
 Flux.@functor PolynomialInterpolation
-Flux.trainable(interp::PolynomialInterpolation) = (interp.x, interp.y)
+Flux.trainable(interp::PolynomialInterpolation) = (;interp.y)
 
 function PolynomialInterpolation(x::AbstractArray, y::AbstractArray)
     mask = I(size(x, 1))
     return PolynomialInterpolation(mask, x, y)
 end
 
+"""
+"""
 function (interp::PolynomialInterpolation)(t::AbstractVector{Float32})
-    # scale = maximum(interp.x, dims = 1)
-    n = interp.mask .+ .!interp.mask .* Flux.unsqueeze((interp.x .- t'), 2)
-    numer = prod(n, dims = 1)
+    scale = Flux.unsqueeze(maximum(abs.(interp.x), dims = 1), 1)
+    n = interp.mask .+ (.!interp.mask) .* Flux.unsqueeze(interp.x .- permutedims(t), 2)
+    numer = Flux.prod(n ./ scale, dims = 1)
 
     T = Flux.unsqueeze(interp.x, 2) .- Flux.unsqueeze(interp.x, 1)
     d = T .+ interp.mask
-    denom = prod(d, dims = 1)
+    denom = Flux.prod(d ./ scale, dims = 1)
     coef = numer ./ denom
-
     return dropdims(sum(interp.y .* coef, dims = 2), dims = 2)
 end
 
 function (dyn::AcousticDynamics{OneDim})(x::AbstractArray, t::AbstractVector{Float32}, θ)
     C, F = θ
+
     U = x[:, 1, :]
     V = x[:, 2, :]
     ∇ = dyn.grad
-
-    dU = (dyn.c0 .^ 2 * C(t)) .* (∇ * V) .- dyn.pml .* U
+    
+    b = dyn.c0 ^ 2 * sigmoid(C(t))    
+    dU = b .* (∇ * V) .- dyn.pml .* U
     dV = ∇ * (U .+ F(t)) .- dyn.pml .* V
 
     return hcat(
@@ -114,7 +117,7 @@ struct GaussianSource <: AbstractSource
 end
 
 Flux.@functor GaussianSource
-Flux.trainable(source::GaussianSource) = (source.μ, source.σ, source.a)
+Flux.trainable(source::GaussianSource) = (;source.μ, source.σ, source.a)
 GaussianSource(dim::AbstractDim, args...) = GaussianSource(build_grid(dim), args...)
 
 function (source::GaussianSource)(t::AbstractVector)
@@ -137,58 +140,87 @@ env = gpu(WaveEnv(dim;
     design_space = Waves.build_triple_ring_design_space(),
     source = source,
     integration_steps = 100,
-    actions = 3))
+    actions = 5))
 
 policy = RandomDesignPolicy(action_space(env))
 # render!(policy, env, path = "vid.mp4")
-ep = generate_episode!(policy, env)
+# ep = generate_episode!(policy, env)
 
-data = Flux.DataLoader(prepare_data([ep, ep], 3), batchsize = 2, shuffle = true, partial = false)
-s, a, t, y = Flux.batch.(first(data))
+data = Flux.DataLoader(prepare_data([ep, ep], 3), batchsize = 3, shuffle = true, partial = false)
+s, a, t, y = gpu(Flux.batch.(first(data)))
 t′ = t[1:env.integration_steps:end, :]
 
 latent_dim = OneDim(15.0f0, 512)
 
 nfreq = 50
 emb = Waves.SinWaveEmbedder(latent_dim, nfreq)
-w = emb(randn(Float32, nfreq, size(t′)...) * 5.0f0)
+w = emb(randn(Float32, nfreq, size(t′)...))
 
-C = Chain(PolynomialInterpolation(t′, w), sigmoid)
-F = GaussianSource(latent_dim, [0.0f0], [0.3f0], [1.0f0], 1000.0f0)
-
-dyn = AcousticDynamics(latent_dim, WATER, 2.0f0, 10000.0f0)
+C = gpu(PolynomialInterpolation(t′, w))
+F = gpu(GaussianSource(latent_dim, [0.0f0], [0.3f0], [1.0f0], 1000.0f0))
+dyn = gpu(AcousticDynamics(latent_dim, WATER, 2.0f0, 10000.0f0))
 iter = gpu(Integrator(runge_kutta, dyn, env.dt))
-θ = gpu([C, F])
-wave = zeros(Float32, size(latent_dim)..., 2, 2)
 
-cost, back = Flux.pullback(θ) do _θ
-    z = iter(wave, t, _θ)
-    return Flux.mean(sum(z[:, 1, 1, :] .^ 2, dims = 1))
-end
-
-gs = back(one(cost))[1]
-
-fig = Figure()
-ax1 = Axis(fig[1, 1])
-ax2 = Axis(fig[1, 2])
-ax3 = Axis(fig[2, 1])
-ax4 = Axis(fig[2, 2])
-lines!(ax1, latent_dim.x, gs[1].layers[1].y[:, 1, 1])
-lines!(ax2, latent_dim.x, gs[1].layers[1].y[:, 2, 1])
-lines!(ax3, latent_dim.x, gs[1].layers[1].y[:, 3, 1])
-lines!(ax4, latent_dim.x, gs[1].layers[1].y[:, 4, 1])
-save("Cgs.png", fig)
+wave = gpu(zeros(Float32, size(latent_dim)..., 2, length(s)))
+C(t[1,:])
 
 
+
+
+
+## testing differentiation through simulation
+# z, back = Flux.pullback(C, F) do _C, _F
+#     return iter(wave, t, [_C, _F])
+# end
+
+# gs = back(z)
+# z = cpu(iter(wave, t, [C, F]))
+
+
+
+
+
+## testing single timestep differentiation
+# θ = [C, F]
+# for i in axes(t, 1)
+#     dwave, back = Flux.pullback(θ) do _θ
+#         return dyn(wave, t[i, :], _θ)
+#     end
+    
+#     gs = back(dwave)[1]
+#     display(i)
+# end
+
+
+
+
+## rendinring simulation
 # fig = Figure()
 # ax = Axis(fig[1, 1])
 # xlims!(ax, latent_dim.x[1], latent_dim.x[end])
 # ylims!(ax, -1.0f0, 1.0f0)
 
 # record(fig, "interp.mp4", axes(t, 1)) do i
-#     empty!(ax)
+#     c = cpu(C(t[i, :]))
 
-#     c = C(t[i, :])[:, 1]
+#     empty!(ax)
+#     lines!(ax, latent_dim.x, c[:, 1], color = :red)
 #     lines!(ax, latent_dim.x, z[:, 1, 1, i], color = :blue)
-#     lines!(ax, latent_dim.x, c, color = :blue)
 # end
+
+
+
+
+# y = sin.(1000.0f0 .* t′)
+# y_true = sin.(1000.0f0 .* t)
+
+# interp = gpu(PolynomialInterpolation(t′, Flux.unsqueeze(y, 1)))
+# y_pred = vcat([interp(t[i, :]) for i in axes(t, 1)]...)
+
+# fig = Figure()
+# ax = Axis(fig[1, 1])
+# scatter!(ax, cpu(t′[:, 1]), cpu(y[:, 1]))
+# lines!(ax, cpu(t[:, 1]), cpu(y_pred[:, 1]), color = :green)
+# lines!(ax, cpu(t[:, 1]), cpu(y_true[:, 1]), color = :orange)
+
+# save("interp.png", fig)

d110f64ac1ccd0959801e8b903188b7d8d5358a5
Tristan Shah <123.tristan.shah@gmail.com>
2024-01-15 20:43:24 -0800

    renaming files

--- scripts/pinn.jl
+++ scripts/pinn.jl
@@ -16,13 +16,21 @@ function (dyn::SimpleWave)(x::AbstractArray{Float32, 3}, t::AbstractVector{Float
 
     u = x[:, 1, :]
     v = x[:, 2, :]
-    u_t = WATER ^ 2 * dyn.grad * v
-    v_t = dyn.grad * (u .+ f)
+    u_t = WATER * dyn.grad * v
+    v_t = WATER * dyn.grad * (u .+ f)
 
     return hcat(
         Flux.unsqueeze(u_t, dims = 2) .* dyn.bc,
         Flux.unsqueeze(v_t, dims = 2)
         )
+end
+
+function runge_kutta_stages(f::AbstractDynamics, u::AbstractArray{Float32}, t, θ, dt::Float32)
+    k1 = f(u,                    t,               θ)
+    k2 = f(u .+ 0.5f0 * dt * k1, t .+ 0.5f0 * dt, θ)
+    k3 = f(u .+ 0.5f0 * dt * k2, t .+ 0.5f0 * dt, θ)
+    k4 = f(u .+ dt * k3,         t .+ dt,         θ)
+    return k1
 end
 
 function build_pinn_grid(latent_dim::OneDim, t::Vector{Float32})
@@ -32,32 +40,19 @@ function build_pinn_grid(latent_dim::OneDim, t::Vector{Float32})
     integration_steps = length(t)
 
     t_grid = repeat(reshape(t, 1, 1, integration_steps), 1, size(latent_dim.x, 1), 1) / (dt * integration_steps)
-    x_grid = gpu(repeat(reshape(latent_dim.x, 1, elements, 1), 1, 1, integration_steps) / latent_gs)
-    pinn_grid = gpu(vcat(x_grid, t_grid))
+    x_grid = repeat(reshape(latent_dim.x, 1, elements, 1), 1, 1, integration_steps) / latent_gs
+    pinn_grid = vcat(x_grid, t_grid)
     return reshape(pinn_grid, 2, :, 1)
-end
-
-"""
-Expects input of size: (2, elements, steps, batch)
-"""
-function compute_wave_physics_loss(z::AbstractArray{Float32, 4}, ∇ₓ, ∇ₜ, f, bc)
-    u = z[1, :, :, 1]
-    v = z[2, :, :, 1]
-
-    u_t = (∇ₜ * u')' .* bc
-    N_u = WATER ^ 2 * ∇ₓ * v .* bc
-    v_t = (∇ₜ * v')'
-    N_v = ∇ₓ * (u .+ f)
-
-    return Flux.mse(u_t, N_u) + Flux.mse(v_t, N_v)
 end
 
 Flux.device!(0)
 
-dt = 1f-7
-integration_steps = 500
-latent_gs = 5.0f0
-elements = 500
+# dt = 5f-6
+# integration_steps = 400
+dt = 1f-5
+integration_steps = 100 #200
+latent_gs = 10.0f0
+elements = 1024
 latent_dim = OneDim(latent_gs, elements)
 dx = get_dx(latent_dim)
 
@@ -68,99 +63,102 @@ dyn = gpu(SimpleWave(
 
 iter = gpu(Integrator(runge_kutta, dyn, dt))
 t = build_tspan(0.0f0, dt, integration_steps)
-
-pinn_grid = build_pinn_grid(latent_dim, t)
-
-wave = zeros(Float32, elements, 2, 1)
-wave[:, 1, :] .= build_normal(latent_dim.x, [0.0f0, 2.0f0], [0.3f0, 0.3f0], [1.0f0, -1.0f0])
-wave = gpu(wave)
+pinn_grid = gpu(build_pinn_grid(latent_dim, t))
 
 grad_x = gpu(Waves.gradient(latent_dim.x))
 grad_t = gpu(Waves.gradient(t))
 
+wave = gpu(zeros(Float32, elements, 2, 1))
 t = gpu(t[:, :])
 
-F = gpu(Source(
-    zeros(Float32, length(latent_dim.x)),
-    # build_normal(latent_dim.x, [0.0f0, 2.0f0], [0.3f0, 0.3f0], [1.0f0, -1.0f0]),
-    1000.0f0))
-
+F = gpu(
+    Source(
+        build_normal(latent_dim.x, [0.0f0], [0.3f0], [1.0f0]),
+        1000.0f0)
+        )
 f = hcat([F(t[i, :]) for i in axes(t, 1)]...)
 
 z = iter(wave, t, F)
 u = z[:, 1, 1, :]
-energy = dropdims(sum(u .^ 2, dims = 1) * dx, dims = 1)
+v = z[:, 2, 1, :]
+energy = vec(sum(u .^ 2, dims = 1) * dx)
 
 h_size = 256
-activation = relu
-U = gpu(
-    Chain(
-        Dense(2, h_size, activation),  
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation),
-        Dense(h_size, h_size, activation),
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation), 
-        Dense(h_size, h_size, activation),
-        Dense(h_size, h_size, activation),  
-        Dense(h_size, 2)))
+activation = leakyrelu
 
-opt_state = Optimisers.setup(Optimisers.Adam(1f-4), U)
+function main()
+    U = gpu(
+        Chain(
+            Dense(2, h_size, activation),  
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation),
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation), 
+            Dense(h_size, h_size, activation),  
+            Dense(h_size, 2)))
+    
+    opt_state = Optimisers.setup(Optimisers.Adam(1f-3), U)
+    # opt_state = Optimisers.setup(Optimisers.Adam(1f-4), U)
 
-for i in 1:300
-    loss, back = Flux.pullback(U) do _U
-        z_pinn = reshape(_U(pinn_grid), 2, elements, integration_steps + 1, :)
-        z_pinn = permutedims(z_pinn, (2, 1, 4, 3))[:, :, 1, :] # (space, fields, time)
-        u_pinn = z_pinn[:, 1, :]
-        v_pinn = z_pinn[:, 2, :]
-        
-        u_pinn_t = (grad_t * u_pinn')'
-        N_u_pinn = WATER * (grad_x * v_pinn)
+    for i in 1:5000
 
-        v_pinn_t = (grad_t * v_pinn')'
-        N_v_pinn = WATER * (grad_x * u_pinn)
+        U_LOSS = nothing
+        V_LOSS = nothing
+        BOUNDARY_LOSS = nothing
+        IC_LOSS = nothing
+        ENERGY_LOSS = nothing
 
-        physics_loss = Flux.mse(u_pinn_t, N_u_pinn)# + Flux.mse(v_pinn_t, N_v_pinn)
-        energy_pinn = dropdims(sum(u_pinn .^ 2, dims = 1) * dx, dims = 1)
-        # z_pinn_next = runge_kutta(dyn, z_pinn, vec(t), F, dt)
-        # physics_loss = Flux.mse(z_pinn[:, :, 2:end], z_pinn_next[:, :, 1:end-1])
-        boundary_loss = Flux.mean(u_pinn[1, :] .^ 2) + Flux.mean(u_pinn[end, :] .^ 2)
-        energy_loss = Flux.mse(energy, energy_pinn)
-        initial_condition_loss = Flux.mse(z_pinn[:, :, 1], z[:, :, 1, 1])
-        return physics_loss + boundary_loss + initial_condition_loss + energy_loss
+        loss, back = Flux.pullback(U) do _U
+            z_pinn = reshape(_U(pinn_grid), 2, elements, integration_steps + 1, :)
+            z_pinn = permutedims(z_pinn, (2, 1, 4, 3))[:, :, 1, :] # (space, fields, time)
+            u_pinn = z_pinn[:, 1, :]
+            v_pinn = z_pinn[:, 2, :]
+
+            u_t = (grad_t * u_pinn')' .* dyn.bc
+            N_u =  WATER * (grad_x * v_pinn) .* dyn.bc
+            v_t = (grad_t * v_pinn')'
+            N_v = WATER * (grad_x * (u_pinn .+ f))
+
+            energy_pinn = vec(sum(u_pinn .^ 2, dims = 1) * dx)
+
+            u_loss = Flux.mse(u_t, N_u) / WATER
+            v_loss = Flux.mse(v_t, N_v) / WATER
+            boundary_loss = Flux.mean(u_pinn[1, :] .^ 2) + Flux.mean(u_pinn[end, :] .^ 2)
+            ic_loss = Flux.mse(z_pinn[:, :, 1], z[:, :, 1, 1])
+            energy_loss = Flux.mse(energy_pinn, energy)
+
+            Flux.ignore() do
+                U_LOSS = u_loss
+                V_LOSS = v_loss
+                BOUNDARY_LOSS = boundary_loss
+                IC_LOSS = ic_loss
+                ENERGY_LOSS = energy_loss
+            end
+
+            return u_loss + v_loss + 1.0f0 * (boundary_loss + 1000.0f0 * ic_loss + energy_loss)
+        end
+
+        println("u: $U_LOSS, v: $V_LOSS, b: $BOUNDARY_LOSS, ic: $IC_LOSS, energy: $ENERGY_LOSS")
+        gs = back(one(loss))[1]
+        opt_state, U = Optimisers.update(opt_state, U, gs)
     end
 
-    println("Loss: $loss")
-    gs = back(one(loss))[1]
-    opt_state, U = Optimisers.update(opt_state, U, gs)
+    return U
 end
 
-
+U = main()
 
 z_pinn = reshape(U(pinn_grid), 2, elements, integration_steps + 1, :)
-z_pinn = permutedims(z_pinn, (2, 1, 4, 3))
-
-fig = Figure()
-ax = Axis(fig[1, 1])
-heatmap!(ax, cpu(z_pinn[:, 1, 1, :]))
-ax = Axis(fig[1, 2])
-heatmap!(ax, cpu(z[:, 1, 1, :]))
-
-ax = Axis(fig[2, 1])
-heatmap!(ax, cpu(z_pinn[:, 2, 1, :]))
-ax = Axis(fig[2, 2])
-heatmap!(ax, cpu(z[:, 2, 1, :]))
-save("z_pinn.png", fig)
-
-u_pinn = z_pinn[:, 1, 1, :]
-energy_pinn = dropdims(sum(u_pinn .^ 2, dims = 1) * dx, dims = 1)
+z_pinn = permutedims(z_pinn, (2, 1, 4, 3))[:, :, 1, :] # (space, fields, time)
+u_pinn = z_pinn[:, 1, :]
+energy_pinn = vec(sum(u_pinn .^ 2, dims = 1) * dx)
 
 fig = Figure()
 ax = Axis(fig[1, 1])
@@ -168,15 +166,20 @@ lines!(ax, cpu(energy))
 lines!(ax, cpu(energy_pinn))
 save("energy.png", fig)
 
-u = cpu(u)
-u_pinn = cpu(u_pinn)
+fig = Figure()
+ax = Axis(fig[1, 1])
+heatmap!(ax, cpu(u))
+ax = Axis(fig[1, 2])
+heatmap!(ax, cpu(u_pinn))
+save("sol.png", fig)
+
 fig = Figure()
 ax = Axis(fig[1, 1])
 xlims!(ax, latent_dim.x[1], latent_dim.x[end])
 ylims!(ax, -2.0f0, 2.0f0)
-
-record(fig, "vid.mp4", axes(u_pinn, 2)) do i
+record(fig, "vid.mp4", axes(u, 2)) do i
     empty!(ax)
-    lines!(ax, latent_dim.x, u[:, i], color = :blue)
-    lines!(ax, latent_dim.x, u_pinn[:, i], color = :orange)
+    lines!(ax, latent_dim.x, cpu(u[:, i]), color = :blue)
+    lines!(ax, latent_dim.x, cpu(u_pinn[:, i]), color = :orange)
 end
+

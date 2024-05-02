a5c530148c2df3b842d7ff59254f57f45fcad05b
Noam Menahem Smilovich <016065699@coe-hpc1.sjsuad.sjsu.edu>
2024-05-01 21:09:07 -0700

    updated train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,12 +5,11 @@
 #SBATCH -p gpu
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 4-00:00:00
-#SBATCH -w cs002
+#SBATCH -t 21-00
+#SBATCH -w cs004
 
 #SBATCH -o logs/log-%j.out
-#SBATCH --mail-user=tristan.shah@sjsu.edu
+#SBATCH --mail-user=noam.smilovich@sjsu.edu
 
-export http_proxy=http://172.16.1.2:3128; export https_proxy=http://172.16.1.2:3128
 module load julia
 srun julia --project scripts/main.jl

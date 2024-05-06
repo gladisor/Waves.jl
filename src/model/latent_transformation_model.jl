84ae0593f5ead49b32924bb3196f93ea406ff452
Noam Menahem Smilovich <016065699@coe-hpc1.sjsuad.sjsu.edu>
2024-05-06 15:09:50 -0700

    added plotting of g functions

--- src/model/latent_transformation_model.jl
+++ src/model/latent_transformation_model.jl
@@ -71,40 +71,67 @@ function make_plots(
     batch; path::String, 
     samples::Int = 1)
 
-s, a, t, y = batch
-z = cpu(generate_latent_solution(model.inner_model, s, a, t))
-latent_dim = cpu(model.inner_model.iter.dynamics.dim)
-render_latent_solution!(latent_dim, cpu(t[:, 1]), z[:, :, 1, :], path = path)
+    s, a, t, y = batch
+    z = cpu(generate_latent_solution(model.inner_model, s, a, t))
+    latent_dim = cpu(model.inner_model.iter.dynamics.dim)
+    render_latent_solution!(latent_dim, cpu(t[:, 1]), z[:, :, 1, :], path = path)
 
-z0, (C, F, PML) = Waves.get_parameters_and_initial_condition(model.inner_model, s, a, t)
+    z0, (C, F, PML) = Waves.get_parameters_and_initial_condition(model.inner_model, s, a, t)
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-lines!(ax, latent_dim.x, cpu(PML[:, 1]))
-save(joinpath(path, "pml.png"), fig)
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+    lines!(ax, latent_dim.x, cpu(PML[:, 1]))
+    save(joinpath(path, "pml.png"), fig)
 
-fig = Figure()
-ax = Axis(fig[1, 1])
-lines!(ax, latent_dim.x, cpu(F.shape[:, 1]))
-save(joinpath(path, "force.png"), fig)
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+    lines!(ax, latent_dim.x, cpu(F.shape[:, 1]))
+    save(joinpath(path, "force.png"), fig)
 
-y_hat = cpu(model(s, a, t))
-y = cpu(y)
-for i in 1:min(length(s), samples)
-    tspan = cpu(t[:, i])
-    plot_predicted_energy(tspan, y[:, 1, i], y_hat[:, 1, i], title = "Total Energy", path = joinpath(path, "tot$i.png"))
-    plot_predicted_energy(tspan, y[:, 2, i], y_hat[:, 2, i], title = "Incident Energy", path = joinpath(path, "inc$i.png"))
-    plot_predicted_energy(tspan, y[:, 3, i], y_hat[:, 3, i], title = "Scattered Energy", path = joinpath(path, "sc$i.png"))
-end
-
-masked_plots_path = mkpath(joinpath(path, "masked_plots"))
-for i in 1:min(length(s), samples)
-    cur_sample_path = mkpath(joinpath(masked_plots_path, "$i"))
-    tspan = cpu(t[:, i])
-    for j in 4:size(y_hat)[2]
-        plot_predicted_energy(tspan, y[:, j, i], y_hat[:, j, i], title = "Scattered Energy in Region $j", path = joinpath(cur_sample_path, "region$j.png"))
+    y_hat = cpu(model(s, a, t))
+    y = cpu(y)
+    for i in 1:min(length(s), samples)
+        tspan = cpu(t[:, i])
+        plot_predicted_energy(tspan, y[:, 1, i], y_hat[:, 1, i], title = "Total Energy", path = joinpath(path, "tot$i.png"))
+        plot_predicted_energy(tspan, y[:, 2, i], y_hat[:, 2, i], title = "Incident Energy", path = joinpath(path, "inc$i.png"))
+        plot_predicted_energy(tspan, y[:, 3, i], y_hat[:, 3, i], title = "Scattered Energy", path = joinpath(path, "sc$i.png"))
     end
+
+    masked_plots_path = mkpath(joinpath(path, "masked_plots"))
+    for i in 1:min(length(s), samples)
+        cur_sample_path = mkpath(joinpath(masked_plots_path, "$i"))
+        tspan = cpu(t[:, i])
+        for j in 4:size(y_hat)[2]
+            plot_predicted_energy(tspan, y[:, j, i], y_hat[:, j, i], title = "Scattered Energy in Region $j", path = joinpath(cur_sample_path, "region$j.png"))
+        end
+    end
+
+    g_plots_path = mkpath(joinpath(path), "g_plots")
+    plot_g_functions(model, path=g_plots_path)
+
+    return nothing
 end
 
-return nothing
+function plot_g_functions(model::LatentTransformationModel; path::String)
+    g_matrix = model.basis * model.coefficients
+    for i in 1:size(g_matrix[1,:])[1]
+        plot_function(g_matrix[:, i], path=joinpath(mkpath(path), "g_$i.png"))
+    end
+    plot_function(vec(sum(g_matrix, dims=2)), path=joinpath(path, "G.png"))
+end
+
+function plot_function(g_function::AbstractVector; path::String)
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+    lines!(ax, 1:size(g_function)[1], g_function)
+    save(path, fig)
+end
+
+function build_encoder()
+    
+    return Metalhead.ViT(:tiny;
+        inchannels = 1,
+        nclasses = 3,
+        imsize = (100, 100),
+        patch_size = (20, 20))
 end
